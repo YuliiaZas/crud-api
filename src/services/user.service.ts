5fc44bafd2a3be43de2d4d97dedab501faef1b78
YuliiaZas <julia.kv101@gmail.com>
2025-05-08 13:44:07 -0400

    feat: lock db for other requests while db is in use

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -2,24 +2,31 @@ import { readFile, writeFile } from 'node:fs/promises';
 import { join } from 'node:path';
 import { randomUUID } from 'crypto';
 import { User } from '../models/user.model';
+import { removeLock, setLock } from '../data/lock';
 
 const FILE_PATH = join(__dirname, '../data/db.json');
 
 export async function getUsers(): Promise<User[]> {
-  const data = await readFile(FILE_PATH, 'utf-8');
-  return JSON.parse(data);
+  await setLock();
+  const users = await readUsers();
+  await removeLock();
+  return users;
 }
 
 export async function createUser(data: Omit<User, 'id'>): Promise<User> {
-  const users = await getUsers();
+  await setLock();
+  const users = await readUsers();
   const newUser: User = { id: randomUUID(), ...data };
   users.push(newUser);
   await writeUsers(users);
+  await removeLock();
   return newUser;
 }
 
 export async function getUserById(id: string): Promise<User | undefined> {
-  const users = await getUsers();
+  await setLock();
+  const users = await readUsers();
+  await removeLock();
   return users.find((user) => user.id === id);
 }
 
@@ -27,24 +34,39 @@ export async function updateUserById(
   id: string,
   data: Omit<User, 'id'>,
 ): Promise<User | undefined> {
-  const users = await getUsers();
+  await setLock();
+  const users = await readUsers();
   const index = users.findIndex((user) => user.id === id);
-  if (index === -1) return undefined;
+  if (index === -1) {
+    await removeLock();
+    return undefined;
+  }
 
   const updatedUser: User = { id, ...data };
   users[index] = updatedUser;
   await writeUsers(users);
+  await removeLock();
   return updatedUser;
 }
 
 export async function deleteUserById(id: string): Promise<boolean> {
-  const users = await getUsers();
+  await setLock();
+  const users = await readUsers();
   const index = users.findIndex((user) => user.id === id);
-  if (index === -1) return false;
+  if (index === -1) {
+    await removeLock();
+    return false;
+  }
 
   users.splice(index, 1);
   await writeUsers(users);
+  await removeLock();
   return true;
+}
+
+async function readUsers(): Promise<User[]> {
+  const data = await readFile(FILE_PATH, 'utf-8');
+  return JSON.parse(data || '[]');
 }
 
 async function writeUsers(users: User[]): Promise<void> {
